fd40a78617cf7f643200ece105797a41913bfc10
haq <1hehaq@gmail.com>
2025-01-17 22:21:14 +0530

    (￣o￣) . z Z

--- pii.sh
+++ pii.sh
@@ -14,4 +14,4 @@ curl -s -G "https://web.archive.org/cdx/search/cdx" \
     --data-urlencode "output=text" \
     --data-urlencode "fl=original" | \
     uro | \
-    grep -E '\.(xls|xml|xlsx|json|pdf|sql|doc|docx|pptx|txt|zip|tar\.gz|tgz|bak|7z|rar|log|cache|secret|db|backup|yml|gz|config|csv|yaml|md|md5|exe|dll|bin|ini|bat|sh|tar|deb|rpm|iso|img|apk|msi|dmg|tmp|crt|pem|key|pub|asc)$'
+    grep -E '\.(xls|git|xlsx|json|pdf|sql|doc|docx|pptx|zip|tar\.gz|tgz|bak|7z|rar|log|cache|secret|db|backup|yml|gz|config|csv|yaml|md|md5|exe|dll|bin|ini|bat|sh|tar|deb|rpm|iso|img|apk|msi|dmg|tmp|crt|pem|key|pub|asc)$'
